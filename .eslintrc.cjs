72841f4e2c256f513a5fa679bb7ce7dae7d7ee24
EXB32665 <german.derbescatoni@claro.com.ar>
2024-07-13 23:26:31 -0300

    chore: modified the pj file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
     require.resolve("@vercel/style-guide/eslint/react"),
     require.resolve("@vercel/style-guide/eslint/next"),
     "plugin:prettier/recommended",
+    "prettier"
   ],
   parserOptions: {
     ecmaVersion: "latest",
